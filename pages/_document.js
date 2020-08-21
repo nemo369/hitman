0f08dba703a1f5f1c18b3f99fd69cd6fd04e35a6
nemo369 <nafrenkel@gmail.com>
2020-08-21 18:08:51 +0300

    SEO mother f*ckr

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
                             __html: `<!-- Global site tag (gtag.js) - Google Analytics --> <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175979529-1"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-175979529-1'); </script>`
                         }}
                     />
+                    <meta property="og:image" content="https://hitman.co.il/hitman.png" />
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <body style={{ backgroundImage: `url(/grid-bg.png)` }}>
