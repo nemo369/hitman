97c0425284711da1ede96b59d43d8ab4cbe15282
nemo369 <nafrenkel@gmail.com>
2020-08-22 19:01:13 +0300

    gtag

--- pages/_document.js
+++ pages/_document.js
@@ -5,23 +5,29 @@ class MyDocument extends Document {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
     }
+    GA_TRACKING_ID = `UA-175979529-1`
 
     render() {
         return (
             <Html dir="rtl">
                 <Head>
 
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
-                        src="https://www.googletagmanager.com/gtag/js?id=UA-175979529-1"
+                        src={`https://www.googletagmanager.com/gtag/js?id=${this.GA_TRACKING_ID}`}
                     />
-
                     <script
-                        dangerouslySetInnerHTML={
-                            {
-                                __html: `window.dataLayer = window.dataLayer || 
-                            [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-175979529-1');`
-                            }}
+                        dangerouslySetInnerHTML={{
+                            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${this.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                        }}
                     />
                     <meta property="og:image" content="https://hitman.co.il/hitman.png" />
                     <link rel="icon" href="/favicon.ico" />
